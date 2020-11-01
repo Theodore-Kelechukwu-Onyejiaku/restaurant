c608f55133d8cae8482d13c78354dd9c34f63b85
unknown <theodoreonyejiaku@gmail.com>
2020-11-01 09:32:23 +0100

    initial setup

---
+++ README.md
@@ -0,0 +1,8 @@
+## Url
+https://theodore-punishment-app.netlify.app/
+
+## Title
+Punishment App
+
+## Description
+A hilarious app that allows you add those you feel like punishing to a punishment list.
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import "bootstrap/dist/css/bootstrap.min.css";
+import  {BrowserRouter as Router, Route} from "react-router-dom";
+import "./App.css"
+
+
+function App(){
+  return (
+      <div className="App">
+        Welcome to React      
+      </div>
+  );
+}
+
+export default App;
