318b34a8d01fbc8b5ca1f81607357f5297e5bddf
unknown <theodoreonyejiaku@gmail.com>
2020-11-10 19:47:59 +0100

    react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import Menu from "./components/MainComponent";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 
 class App extends Component{
 
   render(){
     return (
-      <div>
-        <Menu /> 
-      </div>
+      <BrowserRouter>
+        <div>
+          <Menu /> 
+        </div>
+      </BrowserRouter>
+      
     )
   }
 }
